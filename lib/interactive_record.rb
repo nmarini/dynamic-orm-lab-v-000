afff41c534d7463bff7303fd1f93c6d13b98bd1e
Nicolas Marini <nmarini44@gmail.com>
2018-09-07 00:04:31 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,9 +38,10 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
+      binding.pry
       values << col_name
-    end 
+    end
     values.join(", ")
-  
+
   end
 end
