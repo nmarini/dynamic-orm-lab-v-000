58d12f45b7322693bfcd7f007be7903632e5e3a3
Nicolas Marini <nmarini44@gmail.com>
2018-09-07 00:56:54 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,68 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def initialize(attributes={})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{self.table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |info|
+      column_names << info["name"]
+    end
+    column_names.compact
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if{|col_name| col_name == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+      VALUES (#{values_for_insert})
+      SQL
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(attr_hash)
+    value = attr_hash.values[0]
+    formatted_value = value.class == Integer ? value : "'#{value}'"
+    sql = "SELECT * FROM #{table_name} WHERE #{attr_hash.keys[0]} = #{formatted_value}"
+    DB[:conn].execute(sql)
+
+  end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  self.column_names.each{|name| attr_accessor name.to_sym}
+
 
 end
